1ebd8967a5165dae3e6682d0b270fdf91c822301
Yana541 <149325426+Yana541@users.noreply.github.com>
2024-03-11 14:40:14 +0200

    Create main cpp file

---
+++ eratosthenes.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int>prime(n + 1, 0);
+	for (int i = 2; i <= n; i++)
+	{
+		if (prime[i] == 0)
+			if (i * i * 1ll <= n)
+			{
+				for (int j = i * i; j <= n; j += i)
+				{
+					prime[j] = 1;
+				}
+			}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (prime[i] == 0)
+			cout << i << ' ';
+	}
+}
+
